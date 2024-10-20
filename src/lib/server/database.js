19066dedb37fe23ed7bfc1986d3021e1de4a86b6
kkaushik03 <111904079+kkaushik03@users.noreply.github.com>
2024-10-20 07:40:11 -0700

    fix: resolved db connection and build errors

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,12 +1,9 @@
-import postgres from 'postgres';
 
-// Use environment variables directly from process.env
-const PGCONNECT = process.env.PGCONNECT || process.env.DATABASE_URL;
+import postgres from 'postgres'
 
-if (!PGCONNECT) {
-    throw new Error('Missing database connection string!');
-}
+// see env variables in .env
+import {PGCONNECT} from '$env/static/private';
 
-const sql = postgres(PGCONNECT, {});
+const sql = postgres(PGCONNECT, {} )
 
 export default sql;
